82fdf12ffb56a16ea50a6c0c0fd7684fcb27b81d
David Inglebard RICQ <davidricq87@orange.fr>
2020-11-04 20:51:00 +0100

    Add MAIL_DOMAIN variable.

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,8 +3,14 @@
 usermod -u $WWW_USER_ID www-data
 groupmod -g $WWW_GROUP_ID www-data
 
+final_mail_domain="$(hostname).localhost"
+if [ ! -z "$MAIL_DOMAIN" ]
+then
+      final_mail_domain=$MAIL_DOMAIN
+fi
 
-echo "$(hostname -i)\t$(hostname) $(hostname).localhost" >> /etc/hosts
+
+echo "$(hostname -i)\t$(hostname) $final_mail_domain" >> /etc/hosts
 
 service sendmail start &
 php-fpm
